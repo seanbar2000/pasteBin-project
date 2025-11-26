5f946e470287e10b8daa0f2f14d306fda78914ed
seanbar80 <seanbar80@gmail.com>
2025-11-26 21:55:34 +0200

    latest

---
+++ tests/api_tests.py
@@ -0,0 +1,6 @@
+def get_paste_bin_url():
+    pass
+
+#is mocking means making a "running" api call that returns what u specify?
+#how do i know what exactly to test (a flow? a specific function? the db?)
+#what is a fixture?
